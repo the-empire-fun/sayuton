a0e0733851a41389fbe9f3d1fcee0953223e83d6
habuka036 <osamu@habuka.jp>
2018-10-27 10:48:11 +0900

    add variables instead of hardcoding

--- tools/run.sh
+++ tools/run.sh
@@ -1,7 +1,38 @@
 #!/bin/bash
 
-git clone https://github.com/habuka036/sayuton
-cd sayuton/
-git checkout develop
+missing_args() {
+  echo "Error: option requires an argument -- $1" 1>&2
+  exit 1
+}
+
+for opt in "$@"; do
+  case "$opt" in
+    '--owner' )
+      if [[ -z "$2" ]] || [[ "$2" =~ ^-+ ]]; then
+        missing_args $opt
+      fi
+      owner="$2"
+      shift 2
+      ;;
+    '--repository' )
+      if [[ -z "$2" ]] || [[ "$2" =~ ^-+ ]]; then
+        missing_args $opt
+      fi
+      repository="$2"
+      shift 2
+      ;;
+    '--branch' )
+      if [[ -z "$2" ]] || [[ "$2" =~ ^-+ ]]; then
+        missing_args $opt
+      fi
+      branch="$2"
+      shift 2
+      ;;
+  esac
+done
+
+git clone https://github.com/${owner}/${repository}
+cd ${repository}/
+git checkout $branch
 cd apps/
 python manage.py runserver 0.0.0.0:8000
